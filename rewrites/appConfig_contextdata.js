46148a006ff4361dd805acfed02d8e82a41596e7
lazarga <Nina_Lazar@epam.com>
2015-02-13 14:42:51 +0300

    test

---
+++ rewrites/appConfig_contextdata.js
@@ -0,0 +1,29 @@
+
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "Test Rig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "false",
+		"omnitureOfflineHitLimit" : "2",
+		"demdexEnabled" : "false",
+		"TVEEnabled" : "true",
+		"omnitureContextData" : {
+			"v.appContext1":"test1",
+			"v.appContext2":"test2", 
+			"dummyContext12":"app12",
+			"nick.game": "app1"
+		},
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+	}
+]
+
