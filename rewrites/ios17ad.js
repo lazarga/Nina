b8306e247ea926abf9361214acb76410f152e6ed
lazarga <Nina_Lazar@epam.com>
2015-02-19 16:05:37 +0300

    ios17

---
+++ rewrites/ios17ad.js
@@ -0,0 +1,42 @@
+[
+	{
+		
+        	 "config" : {
+        		 "isCoppaCompliant" : true,
+        		 "audienceManagerEnabled": true
+        	 },
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 250,
+			"appName" : "btg_test_app",
+			"sections" : "/test/mobile300-250",
+			"isDFP" : false,
+			"enableCloseButton" : false,
+			"closeButtonInterval" : 5,
+			"slot"": 21,
+			"isRefreshable" : true,
+			"refreshInterval" : 10,
+			"enableDefaultBumper" : true,
+			"enableCoppaAdBanner" :	true,
+			"keyValues" : {
+				"testkey"   : "key2.1",
+				"testmode" : "celtra_mobile_300x250"
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
